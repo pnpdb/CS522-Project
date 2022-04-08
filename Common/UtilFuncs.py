6cccd88a73184507e4a7d6e0ebcf455f1212c7ca
silver_gp <silver_gp@live.com>
2022-04-08 18:38:56 +0800

    data size

--- Common/UtilFuncs.py
+++ Common/UtilFuncs.py
@@ -1,7 +1,21 @@
-# get the training size of baseline
-def GetTrainSizeBaseline():
-    return [6000, 7500, 12000, 15000, 22500, 30000]
 
-# get training size with noisy data
-def GetTrainSizeWithNoisyData():
-    return [(6000, 1500), (12000, 3000), (22500, 7500)]
+class DataSize:
+    # get the training size of baseline
+    @staticmethod
+    def GetTrainSizeBaseline():
+        return [3000, 6000, 7500, 12000, 15000, 22500, 30000]
+
+    # get training size with noisy data
+    @staticmethod
+    def GetTrainSizeWithNoisyData():
+        #return [(6000, 1500), (12000, 3000), (22500, 7500)]
+        return [(6000, 1500), (12000, 3000)]
+    @staticmethod
+    def GetTestDataSize():
+        return 4000
+    @staticmethod
+    def GetNoiseDataSize():
+        return 3000
+    @staticmethod
+    def GetValidationDataSize():
+        return 1000
