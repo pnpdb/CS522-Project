36aa2d8003caee36feae134e28514126b9bccd20
silver_gp <silver_gp@live.com>
2022-04-09 12:16:10 +0800

    new data size

--- Common/UtilFuncs.py
+++ Common/UtilFuncs.py
@@ -26,12 +26,12 @@ class DataSize:
     # get the training size of baseline
     @staticmethod
     def GetTrainSizeBaseline():
-        return [3000, 6000, 7500, 12000, 15000, 22500, 30000]
+        return [2000, 4000, 5000, 8000, 10000, 15000, 20000]
 
     # get training size with noisy data
     @staticmethod
     def GetTrainSizeWithNoisyData():
-        return [(6000, 1500), (12000, 3000), (22500, 7500)]
+        return [(4000, 1000), (8000, 2000), (15000, 5000)]
 
     @staticmethod
     def GetTestDataSize():
